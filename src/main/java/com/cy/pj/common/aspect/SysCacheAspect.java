90d2435521c66681ce356a3bf73959f33123bef8
tang1997321 <986810860@QQ.COM>
2019-12-11 15:45:51 +0800

    Aop

--- src/main/java/com/cy/pj/common/aspect/SysCacheAspect.java
+++ src/main/java/com/cy/pj/common/aspect/SysCacheAspect.java
@@ -3,16 +3,19 @@ package com.cy.pj.common.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class SysCacheAspect {
-	
-	@Around("@annotation(com.cy.pj.common.annotation.RequiredCache)")
+	@Pointcut("@annotation(com.cy.pj.common.annotation.RequiredCache)")
+	public void doCache(){};
+	@Around("doCache()")
 	public Object around(ProceedingJoinPoint jp) throws Throwable{
 		System.out.println("get data from cache");
 		Object obj = jp.proceed();
+		System.out.println("put data from cache");
 		return obj;
 	}
 }
