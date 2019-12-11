813171c2820e6ee0007733dc52b1efe1a2f8784f
tang1997321 <986810860@QQ.COM>
2019-12-11 11:54:33 +0800

    Aop

--- src/test/java/com/cy/pj/sys/aspect/AopTests.java
+++ src/test/java/com/cy/pj/sys/aspect/AopTests.java
@@ -3,13 +3,11 @@ package com.cy.pj.sys.aspect;
 import com.cy.pj.common.vo.PageObject;
 import com.cy.pj.sys.service.SysUserService;
 import com.cy.pj.sys.vo.SysUserDeptVo;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class AopTests {
 	@Autowired
